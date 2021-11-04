90767c66677782966bfb4ed16fd580675a75e829
stevepodell <steve@podell.com>
2021-11-04 09:31:03 -0700

    new try to get src subdir into loaded lib

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports =  require('./src/Cookie.js')
+module.exports = require('./src/Cookie.js')
+module.exports = require('./src/assign.js')
+module.exports = require('./src/converter.js')
